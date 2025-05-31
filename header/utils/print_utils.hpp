abb9610379f3a4b10fccc0f137529bc0be97f4ad
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-05-31 23:14:53 +0200

    feat(utils): add printLinkedList to print_utils namespace.

--- header/utils/print_utils.hpp
+++ header/utils/print_utils.hpp
@@ -8,6 +8,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <string>
+#include "linked_list/single_linked_list.hpp"
 
 namespace print_utils {
 
@@ -127,6 +128,19 @@ void printBoxedLabel(const std::string& label, int totalWidth = 40) {
     std::cout << "\n+" << std::string(contentWidth, '=') << "+\n";
     std::cout << "| " << label << std::string(contentWidth - label.length(), ' ') << "|\n";
     std::cout << "+" << std::string(contentWidth, '=') << "+\n";
+}
+
+
+void printLinkedList(const LinkedList& list, const std::string& label = "") {
+    if (!label.empty()) std::cout << label << ": ";
+    Node* current = list.head;
+    while (current != nullptr) {
+        std::cout << "[" << current->value << "]";
+        if (current->next != nullptr)
+            std::cout << " -> ";
+        current = current->next;
+    }
+    std::cout << " -> null\n";
 }
 
 
