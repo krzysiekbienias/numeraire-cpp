0e34c7c4a8a69c2250ba32d674d9941d2a605017
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-06-16 14:57:44 +0200

    (feat): Add version of traversing row by row that priont matrix.

--- src/matrix/row_by_row.cpp
+++ src/matrix/row_by_row.cpp
@@ -1,5 +1,6 @@
 #include "matrix/row_by_row.hpp"
 #include<vector>
+#include<iostream>
 // Implement your row_by_row logic here.
 
 std::vector<int> rowByRow (const std::vector <std::vector<int>>& matrix){
@@ -16,4 +17,14 @@ std::vector<int> rowByRow (const std::vector <std::vector<int>>& matrix){
 }
 
 
-
+void rowByRowPrint (const std::vector<std::vector<int>>& matrix){
+    int rows=matrix.size();
+    int cols=matrix[0].size();
+    
+    for (int i=0; i<rows; ++i) {
+        for (int j=0; j<cols; ++j) {
+            std::cout<<matrix[i][j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
