00ea8b80bdf95da6e3bb24ceeb7c3d692b5569bf
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-06-16 14:51:48 +0200

    feat: add optimized two-pointer approach for sorted squares
    - Added new function `sortedSquaredFaster` which uses a two-pointer strategy
      to fill the squared values from back to front, reducing time complexity.

--- src/arrays/sorted_squared.cpp
+++ src/arrays/sorted_squared.cpp
@@ -10,6 +10,27 @@ std::vector<int> sortedSquared(const std::vector<int>& array){
     }
     std::sort(sortedArray.begin(),sortedArray.end());
     return sortedArray;
-    
-    
+}
+
+
+
+std::vector<int> sortedSquaredFaster(const std::vector<int>& array){
+    int n =array.size();
+    std::vector<int> sortedSquared(n);
+    int left=0;
+    int right=n-1;
+    int pos=n-1;//we traverse from back(right)
+    while (left<=right){
+        int leftVal=array[left];
+        int rightVal=array[right];
+        if (abs(leftVal)>abs(rightVal)){
+            sortedSquared[pos--]=leftVal*leftVal;
+            ++left;
+        }
+        else{
+            sortedSquared[pos--]=rightVal*rightVal;
+            --right;
+        }
+    }
+    return sortedSquared;
 }
