e49ef5a069abb03b6dff411f95407a2a2caca8a9
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-06-22 23:31:19 +0200

    feat: Complete implementation to price option using BlackScholesPricer for equities.

--- src/asset_class/equity/black_scholes_pricer.cpp
+++ src/asset_class/equity/black_scholes_pricer.cpp
@@ -1,17 +1,29 @@
 #include "pricing/pricing_factory.hpp"
 #include "core/trade.hpp"
 #include "asset_class/equity/black_scholes_pricer.hpp"
-
+#include "ql/math/distributions/normaldistribution.hpp"
 
 //in well know equations it is allowed to use one letter symbols. It is even clearer to preserve the conventional notation.
+
+double normalCDF(double x) {
+    static QuantLib::CumulativeNormalDistribution phi;
+    return phi(x);
+}
 
 double BlackScholesPricer::price(const PricingFactory& factory) const {
     double K=factory.getStrike();
     double r=factory.getRiskFreeRate();
     double tau=factory.computeYearFraction();
     double St=factory.getUnderlierPrice();
+    double sigma = factory.getVolatility();
     
-    
-    return 0.01;
+    double d1 = (std::log(St / K) + (r + 0.5 * sigma * sigma) * tau) / (sigma * std::sqrt(tau));
+    double d2 = d1 - sigma * std::sqrt(tau);
+    std::string payoff=factory.getPayoff();
+    bool isCall = (payoff == "Call");
+    if (isCall) {
+    } else {
+        return K * std::exp(-r * tau) * normalCDF(-d2) - St * normalCDF(-d1);
+    }
     
 }
