ece2b8ecceaaa5a590f49f4909a9a911992f26cd
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-06-25 23:58:33 +0200

    Add default simulation config constants for paths, window, volatility, and rate.

---
+++ configs/constants.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace config {
+    constexpr int DEFAULT_NUM_PATHS = 1000;
+    constexpr int DEFAULT_HISTORICAL_WINDOW_DAYS = 60;
+    constexpr double DEFAULT_VOLATILITY = 0.2;
+    constexpr double DEFAULT_RISK_FREE_RATE = 0.03;
+}
