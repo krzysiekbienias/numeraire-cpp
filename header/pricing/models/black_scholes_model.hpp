019eb067379a287be551e371fc07bbadc5b0a768
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-06-28 00:58:39 +0200

    feat(black-scholes): add asset-or-nothing option pricing method.

--- header/pricing/models/black_scholes_model.hpp
+++ header/pricing/models/black_scholes_model.hpp
@@ -5,4 +5,5 @@ public :
     double priceVanilla(double S, double K, double r, double sigma, double tau, bool isCall) const;
     
     double priceDigital(double S, double K, double r, double sigma, double tau, double cashPayoff, bool isCall) const;
+    double assetOrNothing(double S, double K, double r, double sigma, double tau, bool isCall) const;
 };
--- src/pricing/models/black_scholes_model.cpp
+++ src/pricing/models/black_scholes_model.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <ql/math/distributions/normaldistribution.hpp>
 
-double normalCDF(double x) {
+double normalCDF(double x) {//TODO move to some math utils
     static QuantLib::CumulativeNormalDistribution phi;
     return phi(x);
 }
@@ -18,3 +18,26 @@ double BlackScholesModel::priceVanilla(double S, double K, double r, double sigm
     }
 }
 
+
+double BlackScholesModel::priceDigital(double S, double K, double r, double sigma, double tau, double cashPayoff, bool isCall) const {
+    double d1 = (std::log(S / K) + (r + 0.5 * sigma * sigma) * tau) / (sigma * std::sqrt(tau));
+    double d2 = d1 - sigma * std::sqrt(tau);
+    
+    if (isCall) {
+        return cashPayoff * std::exp(-r * tau) * normalCDF(d2);
+    } else {
+        return cashPayoff * std::exp(-r * tau) * normalCDF(-d2);
+    }
+}
+
+
+double BlackScholesModel::assetOrNothing(double S, double K, double r, double sigma, double tau, bool isCall) const {
+    double d1 = (std::log(S / K) + (r + 0.5 * sigma * sigma) * tau) / (sigma * std::sqrt(tau));
+    double d2 = d1 - sigma * std::sqrt(tau);
+    
+    if (isCall) {
+        return S*std::exp(-r * tau) * normalCDF(d1);
+    } else {
+        return S*std::exp(-r * tau) * normalCDF(-d1);
+    }
+}
