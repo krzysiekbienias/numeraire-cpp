b4c09177b85196718cefcd78cd36632e37f0d14b
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-07-05 21:24:54 +0200

    fix: fix min_reward and print_utils templates.

--- src/arrays/min_reward.cpp
+++ src/arrays/min_reward.cpp
@@ -1,23 +1,27 @@
 #include<vector>
 #include"arrays/min_reward.hpp"
 #include<numeric>
-#include "utils/print_utils.hpp"
+#include "utils/print_utils_templates.hpp"
 
-// Implement your min_reward logic here.
-    int minReward(const std::vector<int>& scores){
-        std::vector<int> rewards = std::vector<int> (scores.size(),1);
-        //if  current score is bigger than previous one just increase current rewards, with respect to previous
-        for(int i=1;i<scores.size();++i){
-            if (scores[i]>scores[i-1]) {
-                rewards[i]=rewards[i-1]+1;
-            }
+
+int minReward(const std::vector<int>& scores) {
+    std::vector<int> rewards(scores.size(), 1);
+
+    // Left to right
+    for (int i = 1; i < scores.size(); ++i) {
+        if (scores[i] > scores[i - 1]) {
+            rewards[i] = rewards[i - 1] + 1;
         }
-        //if current rewards is bigger than next one then make sure that current is bigger
-        for(int j=scores.size()-1;j=0;--j){
-            if (scores[j]>scores[j+1]){
-                rewards[j]=rewards[j+1]+1;
-            }
+    }
+
+    // Right to left
+    for (int j = scores.size() - 2; j >= 0; --j) {
+        if (scores[j] > scores[j + 1]) {
+            rewards[j] = std::max(rewards[j], rewards[j + 1] + 1);
         }
-//        print_utils::printVector(rewards);
-//        return std::accumulate(rewards.begin(),rewards.end(),0);
+    }
+
+    print_utils::printVector(rewards, "Final Rewards");
+    return std::accumulate(rewards.begin(), rewards.end(), 0);
 }
+
