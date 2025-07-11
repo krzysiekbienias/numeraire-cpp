42d328fbd56cf7cfffd36db82e38e7bf2f3b42bd
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-07-11 23:57:38 +0200

    feat: solve min cost tickets problem using bottom-up dp.

---
+++ header/dynamic_programming/min_cost_for_tickets.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+// Declare your min_cost_for_tickets interface here.
+
+
+int minimumCostTickets(const std::vector<int>& trainDays, const std::vector<int> & costs, int n);
